4fdc9012e475f91e1ccb697286b2bda04dd12703
clove <clove@DESKTOP-M6SD93C>
2020-09-22 22:59:05 +0900

    소영이 공부중 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hi World");
+		System.out.println("소영이 공부중");
+		
 	}
 
 }
